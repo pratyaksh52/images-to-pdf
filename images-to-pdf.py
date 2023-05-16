267ca8828b5eb19329fdc2fd1665fa9d2e16a380
pratyaksh52 <choudharypratyakshpc@gmail.com>
2023-05-17 01:04:23 +0530

    Add functionality to pass source explicitly

--- images-to-pdf.py
+++ images-to-pdf.py
@@ -40,11 +40,10 @@ def convert_to_pdf(read_from, write_to):
 
 
 def main():
-
     # Get the source directory
     if len(sys.argv) != 2:
         sys.exit("Please pass exactly one argument.")
-    
+
     source_path = sys.argv[1]
 
     image_extensions = ("jpeg", "jpg", "png")  # tuple containing image file extensions
@@ -55,15 +54,17 @@ def main():
     for content in os.listdir(source_path):
         if os.path.isdir(content) or (content.split(".")[-1] not in image_extensions):
             continue
-
+        content_path = os.path.join(source_path, content)
         # making a pdf off the current image
         iterable_pdf = "./iterable_pdf_to_be_deleted.pdf"
-        convert_to_pdf(content, iterable_pdf)
+        convert_to_pdf(content_path, iterable_pdf)
 
         # adding this pdf to the PDF object
         combined_pdf_object.append(PdfReader(iterable_pdf))
 
-    combined_pdf_object.write("./final.pdf")  # writing the PDF object as a pdf file
+    combined_pdf_object.write(
+        os.path.join(source_path, "final.pdf")
+    )  # writing the PDF object as a pdf file
     os.remove(iterable_pdf)  # removing the tempoarary pdf
 
 
