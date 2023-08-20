794e9c105c29576e8be59da04eac10de46bd722a
pratyaksh52 <choudharypratyakshpc@gmail.com>
2023-08-20 20:42:48 +0530

    Make use of options instead of positional args

--- images-to-pdf.py
+++ images-to-pdf.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import os, sys
+import argparse
+import os
+import sys
+import traceback
 
 # importing third-party modules
 try:
@@ -14,7 +17,44 @@ except ImportError:
     'pip install -r requirements.txt'"""
     )
 
+    traceback.print_exc()
     sys.exit()
+
+
+def _get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s",
+        "--source_dir",
+        required=True,
+        dest="source_dir",
+        help="Path of the source directory where the images are present",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--dest_path",
+        dest="dest_path",
+        help=(
+            "Path of the directory where the pdf is to be dumped. "
+            "Default is $SOURCE_DIR/final.pdf. "
+            "E.g. /u/username/images-to-pdf/screenshots.pdf."
+        ),
+    )
+
+    args = parser.parse_args()
+    args = _postprocess_args(args)
+    return args
+
+
+def _postprocess_args(args):
+    args.source_dir = os.path.abspath(args.source_dir)
+
+    if not args.dest_path:
+        args.dest_path = os.path.join(args.source_dir, "final.pdf")
+
+    return args
 
 
 def convert_to_pdf(read_from, write_to):
@@ -40,13 +80,14 @@ def convert_to_pdf(read_from, write_to):
 
 
 def main():
+    args = _get_args()
+
     # Get the source directory
-    if len(sys.argv) != 2:
-        sys.exit("Please pass exactly one argument.")
+    source_path = args.source_dir
+    dest_path = args.dest_path
 
-    source_path = sys.argv[1]
-
-    image_extensions = ("jpeg", "jpg", "png")  # tuple containing image file extensions
+    # tuple containing image file extensions
+    image_extensions = ("jpeg", "jpg", "png")
 
     # creating a PDF object which will have the final pdf
     combined_pdf_object = PdfMerger()
@@ -62,9 +103,8 @@ def main():
         # adding this pdf to the PDF object
         combined_pdf_object.append(PdfReader(iterable_pdf))
 
-    combined_pdf_object.write(
-        os.path.join(source_path, "final.pdf")
-    )  # writing the PDF object as a pdf file
+    # writing the PDF object as a pdf file
+    combined_pdf_object.write(os.path.join(dest_path))
     os.remove(iterable_pdf)  # removing the tempoarary pdf
 
 
